b24dbc6fcf193c03597a8f112a6219e585dc1285
Chris Elder <celder0@me.com>
2022-06-10 13:36:15 -0400

    chore: Moved command updating to ready

--- app.js
+++ app.js
@@ -8,36 +8,6 @@ const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MES
 const fs = require("fs");
 
 const token = process.env.TOKEN;
-// const commands = [];
-// const commandFiles = fs.readdirSync('./commands', { withFileTypes: true }).filter(file => file.isDirectory());
-
-// // Place your client and guild ids here
-// const clientId = '982441766697467954';
-// const guildIds = ['980975523175989309', '644627480346492939'];
-
-// for (const file of commandFiles) {
-// 	const command = require(`./commands/${file.name}`);
-// 	commands.push(command.data().toJSON());
-// }
-
-// const rest = new REST({ version: '9' }).setToken(token);
-
-// (async () => {
-//     guildIds.forEach(async(guildId) => {
-//         try {
-//             console.log('Started refreshing application (/) commands.');
-
-//             await rest.put(
-//                 Routes.applicationGuildCommands(clientId, guildId),
-//                 { body: commands },
-//             );
-
-//             console.log('Successfully reloaded application (/) commands.');
-//         } catch (error) {
-//             newrelic.noticeError(error);
-//         }
-//     })
-// })();
 
 // Load Event files from events folder
 const eventFiles = fs.readdirSync('./events/').filter(f => f.endsWith('.js'))
