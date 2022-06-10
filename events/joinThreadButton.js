01e81d26655abea690c2b960f3b8cae3c5650a6b
Chris Elder <celder0@me.com>
2022-06-10 13:34:57 -0400

    fix: Join thread button now sends thread id

---
+++ events/joinThreadButton.js
@@ -0,0 +1,24 @@
+const newrelic = require('newrelic');
+
+async function execute(interaction, bot) {
+    try {
+        if (interaction.customId) {
+            if (interaction.customId.startsWith('joinThread')) {
+                let threadId; 
+                if(interaction.isSelectMenu()) {
+                    threadId = interaction.values[0];    
+                } else {
+                    threadId = interaction.customId.split(':')[1];
+                }
+                const thread = interaction.channel.threads.cache.get(threadId)
+                thread.members.add(interaction.user.id);
+                await interaction.reply({ content: `${thread.name} joined!`, ephemeral: true});
+            }
+        }
+    } catch (e) {
+        console.error(e);
+        newrelic.noticeError(e);
+    }
+}
+
+module.exports = {name: 'interactionCreate', execute}
--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,8 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const fs = require("fs");
+const newrelic = require('newrelic');
+
 module.exports = {
     name: 'ready',
     once: true,
@@ -7,5 +12,40 @@ module.exports = {
 
         //Set the Presence of the bot user
         bot.user.setActivity('screams', {type:'LISTENING'});
+
+        const token = process.env.TOKEN;
+
+        const commands = [];
+        const commandFiles = fs.readdirSync('./commands', { withFileTypes: true }).filter(file => file.isDirectory());
+
+        // Place your client and guild ids here
+        const clientId = bot.application.id;
+        // const guildIds = ['980975523175989309', '644627480346492939'];
+        const guildIds = bot.guilds.cache.map(guild => guild.id);
+
+        for (const file of commandFiles) {
+            const command = require(`../commands/${file.name}`);
+            commands.push(command.data().toJSON());
+        }
+
+        const rest = new REST({ version: '9' }).setToken(token);
+
+        (async () => {
+            guildIds.forEach(async(guildId) => {
+                try {
+                    console.log('Started refreshing application (/) commands.');
+
+                    await rest.put(
+                        Routes.applicationGuildCommands(clientId, guildId),
+                        { body: commands },
+                    );
+
+                    console.log('Successfully reloaded application (/) commands.');
+                } catch (error) {
+                    console.log('Failed to reload application (/) commands \n ' + error)
+                    newrelic.noticeError(error);
+                }
+            })
+        })();
     }
 }
